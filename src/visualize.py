bad4c9ee22617bb5a24c96ea054151bf30713e67
Thomas Peel <thomas.peel@euranova.eu>
2016-07-05 01:33:28 +0200

    Working visualization with thumbnails.

--- src/visualize.py
+++ src/visualize.py
@@ -24,28 +24,27 @@ def plot_embedding(X, title=None):
 
     plt.figure()
     ax = plt.subplot(111)
-    # for i in range(X.shape[0]):
-    #     c = 'r' if i < 60 else 'b'
-    #     plt.scatter(X[i, 0], X[i, 1], c=c)
 
     for i in range(X.shape[0]):
         c = 'r' if i < 50 else 'b'
         plt.text(X[i, 0], X[i, 1], str(i), fontdict={'weight': 'bold', 'size': 9}, color=c)
 
     shown_images = np.array([[1., 1.]])  # just something big
-    # for img in load_image(get_images_path()):
-    #     dist = np.sum((X[i] - shown_images) ** 2, 1)
-    #     if np.min(dist) < 4e-3:
-    #         # don't show points that are too close
-    #         continue
-    #     shown_images = np.r_[shown_images, [X[i]]]
-    #     imagebox = offsetbox.AnnotationBbox(
-    #         offsetbox.OffsetImage(img, cmap=plt.cm.gray_r),
-    #         X[i])
-    #     ax.add_artist(imagebox)
-    # plt.xticks([]), plt.yticks([])
-    # if title is not None:
-    #     plt.title(title)
+    for num, img in enumerate(load_image([path for path in get_images_path() if 'Episode01' in path])):
+        if 11 <= num < 418:
+            i = num - 11
+            dist = np.sum((X[i] - shown_images) ** 2, 1)
+            if np.min(dist) < 4e-3:
+                # don't show points that are too close
+                continue
+            shown_images = np.r_[shown_images, [X[i]]]
+            imagebox = offsetbox.AnnotationBbox(
+                offsetbox.OffsetImage(img, cmap=plt.cm.gray_r),
+                X[i])
+            ax.add_artist(imagebox)
+    plt.xticks([]), plt.yticks([])
+    if title is not None:
+        plt.title(title)
 
 
 if __name__ == '__main__':
